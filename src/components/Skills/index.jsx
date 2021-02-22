f769adc610758e1f251ffa512e49e6e51045083b
D1White <danylo.bilyi@outlook.com>
2021-02-22 15:17:34 +0200

    fix SSR

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,22 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import useMouse from '@react-hook/mouse-position'
 import './skills.scss'
 import { Grid } from '../index'
 import { SkillSection } from './SkillSection'
 import skills from '../../assets/skills.json'
 
+const isBrowser = typeof window !== `undefined`;
+
 const Skills = () => {
   const ref = useRef(null);
-  const [mouse, setMouse] = useState(null)
-  
-  useEffect(() => {
-    setMouse(useMouse(ref, {
-      enterDelay: 100,
-      leaveDelay: 100,
-    }));
-  }, [])
 
+  if (!isBrowser) {
+    global.window = {}; 
+  }
 
+  const mouse = useMouse(ref, {
+    enterDelay: 100,
+    leaveDelay: 100,
+  });
 
   return (
     <div className="skills" ref={ref}>
@@ -24,13 +25,13 @@ const Skills = () => {
         <div className="skills__content">
           <h2 className="block__header">Skills</h2>
           <div className="skills__block">
-            <SkillSection title='Frontend' list={skills.frontend} />
-            <SkillSection title='Backend' list={skills.backend} />
-            <SkillSection title='Other skills' list={skills.other} />
+            <SkillSection title="Frontend" list={skills.frontend} />
+            <SkillSection title="Backend" list={skills.backend} />
+            <SkillSection title="Other skills" list={skills.other} />
           </div>
         </div>
       </div>
-      <Grid mousePos={ mouse ? { x: mouse.x, y: mouse.y } : { x: 0, y: 0 }} />
+      <Grid mousePos={mouse ? { x: mouse.x, y: mouse.y } : { x: 0, y: 0 }} />
     </div>
   )
 }
