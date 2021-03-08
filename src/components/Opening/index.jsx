291aa52c6060ea3d5230f923589a9cf3ac9ca7a0
D1White <danylo.bilyi@outlook.com>
2021-03-08 19:30:44 +0200

    add adaptive styles

--- src/components/Opening/index.jsx
+++ src/components/Opening/index.jsx
@@ -8,14 +8,16 @@ const Opening = () => {
     <div className="opening" id='opening'>
       <div className="container">
         <div className="opening__content">
-          <img src={avatar} alt="avatar" className="opening__avatar" draggable="false" />
-          <span className="opening__subtitle" role="img" aria-label="waving hand">
-            Hi, I'm Danylo Bilyi 👋
-          </span>
-          <h1 className="opening__title">
-            Front-end developer passionate about creating beautiful
-            user-friendly apps
-          </h1>
+          <div className="opening__info">
+            <img src={avatar} alt="avatar" className="opening__avatar" draggable="false" />
+            <span className="opening__subtitle" role="img" aria-label="waving hand">
+              Hi, I'm Danylo Bilyi 👋
+            </span>
+            <h1 className="opening__title">
+              Front-end developer passionate about creating beautiful
+              user-friendly apps
+            </h1>
+          </div>
           <div className="opening__arrow">
             <ArrowIcon />
           </div>
